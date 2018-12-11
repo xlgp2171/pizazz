d2521409c6939831a7241c653a754d6c26f31a18
xlgp2171 <xlgp2171@163.com>
2018-12-11 14:45:54 +0800

    基础代码

---
+++ base/src/main/java/org/pizazz/Constant.java
@@ -0,0 +1,13 @@
+package org.pizazz;
+
+/**
+ * 全局常量
+ * 
+ * @author xlgp2171
+ * @version 1.0.181210
+ */
+public final class Constant {
+
+	public static final String NAMING = "pizazz";
+	public static final String NAMING_SHORT = "piz";
+}
