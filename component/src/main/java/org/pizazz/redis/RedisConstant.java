9c50693095a7f0f04e7336d5dd46639176ae54f7
xlgp2171 <xlgp2171@163.com>
2019-01-03 11:02:00 +0800

    新增根节点常量

--- component/src/main/java/org/pizazz/redis/RedisConstant.java
+++ component/src/main/java/org/pizazz/redis/RedisConstant.java
@@ -2,6 +2,7 @@ package org.pizazz.redis;
 
 public class RedisConstant {
 
+	public static final String KEY_REDIS = "redis";
 	public static final String KEY_CLIENT = "client";
 	public static final String KEY_CONFIG = "config";
 }
