8a3003ece6f1dfdf885fe769457ca38125cd9a7d
xlgp2171 <xlgp2171@163.com>
2019-01-03 11:01:35 +0800

    优化异常提示

--- component/src/main/java/org/pizazz/redis/IRedisProcessor.java
+++ component/src/main/java/org/pizazz/redis/IRedisProcessor.java
@@ -2,17 +2,19 @@ package org.pizazz.redis;
 
 import java.util.Map;
 
+import org.pizazz.redis.exception.RedisException;
+
 public interface IRedisProcessor {
-	public void set(String key, String value);
-	public void bset(String key, byte[] value);
-	public void hmset(String key, Map<String, String> map);
-	public void hset(String key, String field, String value);
+	public void set(String key, String value) throws RedisException;
+	public void bset(String key, byte[] value) throws RedisException;
+	public void hmset(String key, Map<String, String> map) throws RedisException;
+	public void hset(String key, String field, String value) throws RedisException;
 
-	public String get(String key);
-	public byte[] bget(String key);
-	public String hget(String key, String field);
-	public Map<String, String> hmget(String key);
+	public String get(String key) throws RedisException;
+	public byte[] bget(String key) throws RedisException;
+	public String hget(String key, String field) throws RedisException;
+	public Map<String, String> hmget(String key) throws RedisException;
 
-	public String hdel(String key, String field);
-	public boolean del(String key);
+	public String hdel(String key, String field) throws RedisException;
+	public boolean del(String key) throws RedisException;
 }
