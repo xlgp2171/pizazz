83174d66295b0bf39ab8f49cc96569e7097f853d
xlgp2171 <xlgp2171@163.com>
2019-02-20 16:01:16 +0800

    新增异常构造

--- base/src/main/java/org/pizazz/exception/BaseException.java
+++ base/src/main/java/org/pizazz/exception/BaseException.java
@@ -6,7 +6,7 @@ import org.pizazz.message.ref.IMessageCode;
  * 基础异常
  * 
  * @author xlgp2171
- * @version 1.0.181210
+ * @version 1.1.190220
  * 
  * @see IMessageCode
  */
@@ -19,6 +19,10 @@ public class BaseException extends AbstractException {
 
 	public BaseException(IMessageCode code, Throwable cause) {
 		super(code, cause);
+	}
+
+	public BaseException(String message, Throwable cause) {
+		super(message, cause);
 	}
 
 	public BaseException(IMessageCode code, String message, Throwable cause) {
--- base/src/main/java/org/pizazz/exception/ToolException.java
+++ base/src/main/java/org/pizazz/exception/ToolException.java
@@ -6,13 +6,17 @@ import org.pizazz.message.ref.IMessageCode;
  * 实用工具使用异常
  * 
  * @author xlgp2171
- * @version 1.0.190219
+ * @version 1.0.190220
  * 
  * @see IMessageCode
  */
 public class ToolException extends BaseException {
 	private static final long serialVersionUID = 1L;
 
+	public ToolException(String message, Throwable cause) {
+		super(message, cause);
+	}
+	
 	public ToolException(IMessageCode code, String message) {
 		super(code, message);
 	}
