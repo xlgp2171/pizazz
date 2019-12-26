c4ab53ed4ddf97e549cd89550db907812be94d94
xlgp2 <xlgp2@pizazz2>
2019-12-26 21:49:53 +0800

    基础代码

--- component/src/main/java/org/pizazz/redis/RedisHelper.java
+++ component/src/main/java/org/pizazz/redis/RedisHelper.java
@@ -2,15 +2,13 @@ package org.pizazz.redis;
 
 import java.nio.charset.StandardCharsets;
 
-import org.pizazz.common.BytesUtils;
-
 public class RedisHelper {
 
 	public static String toString(byte[] target) {
-		return BytesUtils.toString(target, StandardCharsets.UTF_8);
+		return new String(target, StandardCharsets.UTF_8);
 	}
 
 	public static byte[] fromString(String target) {
-		return BytesUtils.toByteArray(target, StandardCharsets.UTF_8);
+		return target.getBytes(StandardCharsets.UTF_8);
 	}
 }
