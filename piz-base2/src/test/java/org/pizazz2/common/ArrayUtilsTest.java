88e2829724475beeabfbba7cf86484453e4066d8
xlgp2171 <xlgp2171@163.com>
2021-03-11 00:57:17 +0800

    初始化2.0版本

---
+++ piz-base2/src/test/java/org/pizazz2/common/ArrayUtilsTest.java
@@ -0,0 +1,57 @@
+package org.pizazz2.common;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * ArrayUtils测试
+ *
+ * @author xlgp2171
+ * @version 2.0.210201
+ */
+public class ArrayUtilsTest {
+    @Test
+    public void testIsEmpty() {
+        Object[] target = new Object[] {};
+        boolean result = ArrayUtils.isEmpty(target);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void testInsert() {
+        Object[] target = new Object[] { "A", "B", "C" };
+        int index = 2;
+        String element = "X";
+        Object[] result = ArrayUtils.insert(target, index, element);
+        Assert.assertArrayEquals(result, new String[] { "A", "B", "X", "C" });
+    }
+
+    @Test
+    public void testContains() {
+        Object[] target = new Object[] { "A", "B", "2", "C" };
+        String element = "2";
+        boolean result = ArrayUtils.contains(target, element);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void testNullToEmpty() {
+        Object[] target = null;
+        target = ArrayUtils.nullToEmpty(target);
+        Assert.assertArrayEquals(target, new Object[] {});
+    }
+
+    @Test
+    public void testNewArray() {
+        String[] result = ArrayUtils.newArray(3, "E");
+        Assert.assertArrayEquals(result, new String[] { "E", "E", "E" });
+    }
+
+    @Test
+    public void testMerge() {
+        String[] right = new String[] { "1", "2" };
+        String[] left = new String[] { "A", "B", "C" };
+        String[] result = ArrayUtils.merge(left, right);
+        Assert.assertArrayEquals(result, new String[] { "A", "B", "C", "1", "2" });
+    }
+}
