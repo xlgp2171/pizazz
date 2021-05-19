3f256a58902787dd390a2d03140cb0102fe4ef2a
xlgp2171 <xlgp2171@163.com>
2021-05-19 19:33:56 +0800

    优化json组件

--- piz-base2/src/main/java/org/pizazz2/common/JSONUtils.java
+++ piz-base2/src/main/java/org/pizazz2/common/JSONUtils.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 /**
  * 对象JSON处理
- * <li/>使用jackson组件
+ * <li/>使用fastjson组件
  *
  * @author xlgp2171
  * @version 2.0.210512
@@ -23,40 +23,6 @@ public class JSONUtils {
     static {
         EMPTY_JSON = JSONUtils.toJSON(TupleObjectHelper.emptyObject());
     }
-
-    /*
-    public static String toJSON(Object target, IJacksonConfig config) throws UtilityException {
-        if (target == null) {
-            return "{}";
-        } else if (config == null) {
-            config = IJacksonConfig.EMPTY;
-        }
-        com.fasterxml.jackson.databind.ObjectMapper mapper = new com.fasterxml.jackson.databind.ObjectMapper(config.factory());
-        config.set(mapper);
-        try {
-            return mapper.writeValueAsString(target);
-        } catch (com.fasterxml.jackson.core.JsonProcessingException e) {
-            String msg = LocaleHelper.toLocaleText(TypeEnum.BASIC, "ERR.JACKSON.PROCESS", e.getMessage());
-            throw new UtilityException(BasicCodeEnum.MSG_0013, msg, e);
-        }
-    }
-
-    public static <T> T fromJSON(String target, Class<T> type, IJacksonConfig config) throws ValidateException, UtilityException {
-        ValidateUtils.notNull("fromJSON", target, type);
-
-        if (config == null) {
-            config = IJacksonConfig.EMPTY;
-        }
-        com.fasterxml.jackson.databind.ObjectMapper mapper = new com.fasterxml.jackson.databind.ObjectMapper(config.factory());
-        config.set(mapper);
-        try {
-            return mapper.readValue(target, type);
-        } catch (IOException e) {
-            String msg = LocaleHelper.toLocaleText(TypeEnum.BASIC, "ERR.JACKSON.PROCESS", e.getMessage());
-            throw new UtilityException(BasicCodeEnum.MSG_0013, msg, e);
-        }
-    }
-     */
 
     public static String toJSON(Object target) throws ValidateException {
         return JSONUtils.toJSON(target, false);
