b33802c72c4368ecf491b40416eb69b19966e769
xlgp2171 <xlgp2171@163.com>
2021-05-19 19:32:32 +0800

    新增json array处理方法

--- piz-base2/src/main/java/org/pizazz2/common/JSONUtils.java
+++ piz-base2/src/main/java/org/pizazz2/common/JSONUtils.java
@@ -3,17 +3,26 @@ package org.pizazz2.common;
 import com.alibaba.fastjson.JSONException;
 import org.pizazz2.exception.ValidateException;
 import org.pizazz2.helper.LocaleHelper;
+import org.pizazz2.helper.TupleObjectHelper;
 import org.pizazz2.message.BasicCodeEnum;
 import org.pizazz2.message.TypeEnum;
+
+import java.util.List;
 
 /**
  * 对象JSON处理
  * <li/>使用jackson组件
  *
  * @author xlgp2171
- * @version 2.0.210201
+ * @version 2.0.210512
  */
 public class JSONUtils {
+    /** 空的JSON字符串 */
+    public final static String EMPTY_JSON;
+
+    static {
+        EMPTY_JSON = JSONUtils.toJSON(TupleObjectHelper.emptyObject());
+    }
 
     /*
     public static String toJSON(Object target, IJacksonConfig config) throws UtilityException {
@@ -50,10 +59,13 @@ public class JSONUtils {
      */
 
     public static String toJSON(Object target) throws ValidateException {
-        // return toJSON(target, null);
+        return JSONUtils.toJSON(target, false);
+    }
+
+    public static String toJSON(Object target, boolean prettyFormat) throws ValidateException {
         // 默认采用fastjson，支持大对象处理
         try {
-            return com.alibaba.fastjson.JSONObject.toJSONString(target);
+            return com.alibaba.fastjson.JSONObject.toJSONString(target, prettyFormat);
         } catch (JSONException e) {
             String msg = LocaleHelper.toLocaleText(TypeEnum.BASIC, "ERR.FASTJSON.PROCESS", e.getMessage());
             throw new ValidateException(BasicCodeEnum.MSG_0013, msg, e);
@@ -65,6 +77,16 @@ public class JSONUtils {
         // 默认采用fastjson，支持大对象处理
         try {
             return com.alibaba.fastjson.JSONObject.parseObject(target, type);
+        } catch (JSONException e) {
+            String msg = LocaleHelper.toLocaleText(TypeEnum.BASIC, "ERR.FASTJSON.PROCESS", e.getMessage());
+            throw new ValidateException(BasicCodeEnum.MSG_0013, msg, e);
+        }
+    }
+
+    public static <T> List<T> fromJSONArray(String target, Class<T> type) {
+        // 默认采用fastjson，支持大对象处理
+        try {
+            return com.alibaba.fastjson.JSONObject.parseArray(target, type);
         } catch (JSONException e) {
             String msg = LocaleHelper.toLocaleText(TypeEnum.BASIC, "ERR.FASTJSON.PROCESS", e.getMessage());
             throw new ValidateException(BasicCodeEnum.MSG_0013, msg, e);
