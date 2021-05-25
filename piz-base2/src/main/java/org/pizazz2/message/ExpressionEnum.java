5a2f29dfefee5ea42a8407f9d52d0b08f1090bc5
xlgp2171 <xlgp2171@163.com>
2021-05-26 01:01:49 +0800

    优化正则表达式

--- piz-base2/src/main/java/org/pizazz2/message/ExpressionEnum.java
+++ piz-base2/src/main/java/org/pizazz2/message/ExpressionEnum.java
@@ -1,9 +1,5 @@
 package org.pizazz2.message;
 
-import org.pizazz2.data.TupleObject;
-
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.regex.Pattern;
@@ -12,13 +8,17 @@ import java.util.regex.Pattern;
  * 表达式常量
  *
  * @author xlgp2171
- * @version 2.0.200201
+ * @version 2.1.200525
  */
 public enum ExpressionEnum {
     /**
+     * 仅ACSII字符
+     */
+    ASCII("^[\\x00-\\xFF]+$"),
+    /**
      * 匹配中文字符的正则表达式
      */
-    CHINESE("[\\u4e00-\\u9fa5]+"),
+    CHINESE("^[\\u4E00-\\u9FA5\\uF900-\\uFA2D]+$"),
     /**
      * 验证年龄
      */
@@ -26,7 +26,7 @@ public enum ExpressionEnum {
     /**
      * 中文或英文
      */
-    CN_OR_EN("[\\u4e00-\\u9fa5a-zA-Z0-9]+"),
+    CN_OR_EN("[\\u4E00-\\u9FA5\\uF900-\\uFA2Da-zA-Z0-9]+"),
     /**
      * 匹配HTML标记的正则表达式
      */
@@ -38,19 +38,19 @@ public enum ExpressionEnum {
     /**
      * 匹配Email地址的正则表达式
      */
-    EMAIL("^\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$"),
+    EMAIL("^\\w+((-\\w+)|(\\.\\w+))*\\@[A-Za-z0-9]+((\\.|-)[A-Za-z0-9]+)*\\.[A-Za-z0-9]+$"),
     /**
-     * 匹配网址URL的正则表达式:网上流传的版本功能很有限，这个基本可以满足需求
+     * 匹配网址URL的正则表达式
      */
-    URL("[a-zA-z]+://[^\\s]*"),
+    URL("^http[s]?=\\/\\/([\\w-]+\\.)+[\\w-]+([\\w-./?%&=]*)?$"),
     /**
      * 匹配帐号是否合法(字母开头，允许5-16字节，允许字母数字下划线)
      */
     ACCOUNT("^[a-zA-Z][a-zA-Z0-9_]{2,15}$"),
     /**
-     * 匹配国内固定电话号码
+     * 电话号码的函数(包括验证国内区号;国际区号;分机号)
      */
-    TEL_PHONE("^\\d{3}-\\d{8}|\\d{4}-\\d{7,8}$"),
+    TEL_PHONE("^(([0\\+]\\d{2,3}-)?(0\\d{2,3})-)?(\\d{7,8})(-(\\d{1,}))?$"),
     /**
      * 腾讯QQ号从10000开始
      */
@@ -62,11 +62,11 @@ public enum ExpressionEnum {
     /**
      * 匹配身份证
      */
-    ID_CARD_NO("^\\d{15}|\\d{18}$"),
+    ID_CARD_NO("^[1-9]([0-9]{14}|[0-9]{17})$"),
     /**
      * 匹配ip地址
      */
-    IP_ADDRESS("\\d+\\.\\d+\\.\\d+\\.\\d+"),
+    IP_ADDRESS("^(25[0-5]|2[0-4]\\d|[0-1]\\d{2}|[1-9]?\\d)\\.(25[0-5]|2[0-4]\\d|[0-1]\\d{2}|[1-9]?\\d)\\.(25[0-5]|2[0-4]\\d|[0-1]\\d{2}|[1-9]?\\d)\\.(25[0-5]|2[0-4]\\d|[0-1]\\d{2}|[1-9]?\\d)$"),
     /**
      * 匹配由26个英文字母组成的字符串
      */
@@ -104,7 +104,7 @@ public enum ExpressionEnum {
     /**
      * 验证手机号码
      */
-    PHONE("^(13|15)\\d{9}$"),
+    MOBILE("^(13|14|15|16|17|18|19)[0-9]{9}$"),
     /**
      * 匹配包名称
      */
@@ -112,13 +112,17 @@ public enum ExpressionEnum {
     /**
      * 匹配类名称
      */
-    CLASS_NAME("class\\s+([$_a-zA-Z][$_a-zA-Z0-9]*)[\\s<\\{]+");
+    CLASS_NAME("class\\s+([$_a-zA-Z][$_a-zA-Z0-9]*)[\\s<\\{]+"),
+    /**
+     * 颜色
+     */
+    COLOR("^[a-fA-F0-9]{6}$");
 
     static final ConcurrentMap<ExpressionEnum, Pattern> CACHE = new ConcurrentHashMap<>();
 
     private final String value;
 
-    private ExpressionEnum(String value) {
+    ExpressionEnum(String value) {
         this.value = value;
     }
 
