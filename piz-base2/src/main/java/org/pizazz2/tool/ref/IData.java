56677b3c647845623eb7868d781e5d5d88a4a32a
xlgp2171 <xlgp2171@163.com>
2021-08-27 16:34:21 +0800

    新增数据流处理器的数据元接口

---
+++ piz-base2/src/main/java/org/pizazz2/tool/ref/IData.java
@@ -0,0 +1,16 @@
+package org.pizazz2.tool.ref;
+
+/**
+ * 批量数据对象
+ *
+ * @author xlgp2171
+ * @version 2.0.210827
+ */
+public interface IData {
+    /**
+     * 数据长度
+     *
+     * @return 数据长度
+     */
+    int length();
+}
