28d243969a7a41d0690e25441d433a128448c6a2
xlgp2171 <xlgp2171@163.com>
2021-10-13 18:38:06 +0800

    新增普通消息输出

--- piz-base2/src/main/java/org/pizazz2/exception/UtilityException.java
+++ piz-base2/src/main/java/org/pizazz2/exception/UtilityException.java
@@ -6,12 +6,16 @@ import org.pizazz2.message.ref.IMessageCode;
  * 通用工具使用异常
  * 
  * @author xlgp2171
- * @version 2.0.210201
+ * @version 2.1.210917
  * 
  * @see IMessageCode
  */
 public class UtilityException extends BaseException {
 	private static final long serialVersionUID = 7798259423596221431L;
+
+	public UtilityException(String message, Throwable cause) {
+		super(message, cause);
+	}
 
 	public UtilityException(IMessageCode code, String message) {
 		super(code, message);
