543d9aafc0134cb6e09948ecb3e06253a828500d
xlgp2171 <xlgp2171@163.com>
2021-10-13 18:37:42 +0800

    修复消息码为0000带来的消息异常输出

--- piz-base2/src/main/java/org/pizazz2/exception/ValidateException.java
+++ piz-base2/src/main/java/org/pizazz2/exception/ValidateException.java
@@ -4,14 +4,15 @@ import org.pizazz2.message.BasicCodeEnum;
 import org.pizazz2.message.ref.IMessageCode;
 
 /**
- * 断言验证异常
+ * 断言验证运行时异常
  * 
  * @author xlgp2171
- * @version 2.0.210201
+ * @version 2.1.210917
  * 
  * @see IMessageCode
  */
 public class ValidateException extends RuntimeException implements IException {
+
 	private static final long serialVersionUID = 7710998580948211094L;
 	private final IMessageCode code;
 
@@ -47,6 +48,7 @@ public class ValidateException extends RuntimeException implements IException {
 
 	@Override
 	public String getMessage() {
-		return code == null ? getNativeMessage() : code.append(getNativeMessage()).toString();
+		return code == null || code == BasicCodeEnum.MSG_0000 ?
+				getNativeMessage() : code.append(getNativeMessage()).toString();
 	}
 }
